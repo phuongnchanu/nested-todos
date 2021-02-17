41097511ceefc0645e528c59601e5e4744522044
Phuong Nguyen <phuongnc4c13@s.hanu.edu.vn>
2021-02-17 23:30:43 +0700

    Remove unneeded variable assignment

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@
   document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
   var model = new window.app.Model('nested-todo');
   var view = new window.app.View();
-  var controller = new window.app.Controller(model, view);
+  new window.app.Controller(model, view);
 })();
