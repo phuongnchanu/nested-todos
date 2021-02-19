3e75cdcfb792b32d8fca4b3bd2890d155791b4de
Phuong Nguyen <phuongnc4c13@s.hanu.edu.vn>
2021-02-19 21:03:30 +0700

    Fix dialogs flash when the page first loads

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 (function() {
-  document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
   var model = new window.app.Model('nested-todo');
   var view = new window.app.View();
   new window.app.Controller(model, view);
